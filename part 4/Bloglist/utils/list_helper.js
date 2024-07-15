f78babbec86ba1fe7a0a3a5fc3458cb3a7cece72
ZiqiLi28 <shiyuyuanf@outlook.com>
2024-07-15 13:59:34 +0300

    e 4.6

--- part 4/Bloglist/utils/list_helper.js
+++ part 4/Bloglist/utils/list_helper.js
@@ -1,12 +1,30 @@
 const dummy = (blogs) => {
-    return 1
+  return 1
+}
+
+const totalLikes = (blogs) => {
+  return blogs.reduce((sum, blog) => sum + blog.likes, 0)
+}
+
+const mostBlogs = (blogs) => {
+  if (blogs.length === 0) {
+    return null
   }
-  
-  const totalLikes = (blogs) => {
-    return blogs.reduce((sum, blog) => sum + blog.likes, 0)
-  }
-  
-  module.exports = {
-    dummy,
-    totalLikes
-  }
+
+  const authorCount = blogs.reduce((acc, blog) => {
+    acc[blog.author] = (acc[blog.author] || 0) + 1
+    return acc
+  }, {})
+
+  const topAuthor = Object.keys(authorCount).reduce((top, author) => {
+    return authorCount[author] > top.blogs ? { author, blogs: authorCount[author] } : top
+  }, { author: '', blogs: 0 })
+
+  return topAuthor
+}
+
+module.exports = {
+  dummy,
+  totalLikes,
+  mostBlogs
+}
