952a50c7d70d426412d052915cf3ed524b395e36
ZiqiLi28 <shiyuyuanf@outlook.com>
2024-07-25 15:28:29 +0300

    e 4.19

--- part 4/Bloglist/controllers/blog.js
+++ part 4/Bloglist/controllers/blog.js
@@ -1,4 +1,16 @@
-blogsRouter.post('/', async (request, response) => {
+const blogsRouter = require('express').Router()
+const Blog = require('../models/blog')
+const User = require('../models/user')
+const { userExtractor } = require('../utils/middleware')
+
+// 获取所有博客
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
+  response.json(blogs)
+})
+
+// 创建新博客
+blogsRouter.post('/', userExtractor, async (request, response) => {
   const body = request.body
   const user = request.user
 
@@ -20,3 +32,33 @@ blogsRouter.post('/', async (request, response) => {
 
   response.status(201).json(savedBlog)
 })
+
+// 删除博客
+blogsRouter.delete('/:id', userExtractor, async (request, response) => {
+  const blog = await Blog.findById(request.params.id)
+  const user = request.user
+
+  if (blog.user.toString() !== user.id.toString()) {
+    return response.status(401).json({ error: 'only the creator can delete a blog' })
+  }
+
+  await Blog.findByIdAndRemove(request.params.id)
+  response.status(204).end()
+})
+
+// 更新博客
+blogsRouter.put('/:id', async (request, response) => {
+  const body = request.body
+
+  const blog = {
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+  }
+
+  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+  response.json(updatedBlog)
+})
+
+module.exports = blogsRouter
--- part 4/Bloglist/test/blog_api.test.js
+++ part 4/Bloglist/test/blog_api.test.js
@@ -1,12 +1,12 @@
-const jwt = require('jsonwebtoken')
-const User = require('../models/user')
-const Blog = require('../models/blog')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
 const api = supertest(app)
-
-let token = ''
+const Blog = require('../models/blog')
+const User = require('../models/user')
+const helper = require('./test_helper')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
 beforeEach(async () => {
   await User.deleteMany({})
@@ -21,10 +21,30 @@ beforeEach(async () => {
     id: user._id,
   }
 
-  token = jwt.sign(userForToken, process.env.SECRET, { expiresIn: '1h' })
+  const token = jwt.sign(userForToken, process.env.SECRET, { expiresIn: 60*60 })
+
+  await api
+    .post('/api/blogs')
+    .send({
+      title: 'initial blog',
+      author: 'author',
+      url: 'http://example.com',
+      likes: 0,
+    })
+    .set('Authorization', `Bearer ${token}`)
 })
 
-test('a valid blog can be added', async () => {
+test('a valid blog can be added only with a valid token', async () => {
+  const users = await helper.usersInDb()
+  const user = users[0]
+
+  const userForToken = {
+    username: user.username,
+    id: user.id,
+  }
+
+  const token = jwt.sign(userForToken, process.env.SECRET, { expiresIn: 60*60 })
+
   const newBlog = {
     title: 'New Blog',
     author: 'Author',
@@ -39,11 +59,28 @@ test('a valid blog can be added', async () => {
     .expect(201)
     .expect('Content-Type', /application\/json/)
 
-  const blogsAtEnd = await Blog.find({}).populate('user', { username: 1, name: 1 })
-  expect(blogsAtEnd).toHaveLength(1)
+  const blogsAtEnd = await helper.blogsInDb()
+  expect(blogsAtEnd).toHaveLength(2)
 
   const contents = blogsAtEnd.map(b => b.title)
   expect(contents).toContain('New Blog')
+})
+
+test('a blog cannot be added without a token', async () => {
+  const newBlog = {
+    title: 'Unauthorized Blog',
+    author: 'Author',
+    url: 'http://example.com',
+    likes: 5,
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(401)
+
+  const blogsAtEnd = await helper.blogsInDb()
+  expect(blogsAtEnd).toHaveLength(1)
 })
 
 afterAll(() => {
